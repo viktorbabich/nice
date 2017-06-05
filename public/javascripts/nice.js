b00e1407b0b2632aae8fb0c9c6628de10c3c9ed3
viktorbabich <victorbabich@mail.ru>
2017-06-06 01:08:15 +0500

    get all files together

--- public/javascripts/nice.js
+++ public/javascripts/nice.js
@@ -1,121 +1,184 @@
-// var app = {
-// 	beforeAsk: function() {
-// 		работает за компом периодически кидая рандомные фразы
-// 		от 1 до 3
-// 	},
-// 	load: function() {
-// 		let turn = Math.random();
-// 		if(turn) {
-// 			app.turn();
-// 		} else {
-// 			question = "Что такое..?";
-
-// 			Щас...щас минутку... течение 3 секунд
-
-// 			Щас...
-
-// 			Минутку...
-
-// 			setTimeout(app.turn(), 5000);
-// 		};	
-// 	},
-// 	turn: function () {
-// 		поворачивается
-// 		app.ask();
-// 	},
-// 	ask: function() {
-// 		задает вопрос
-// 		выезжает попап с инпутом
-// 		на ентер или по нажатию кнопки
-// 		joke.showTable();
-// 	},
-// 	answear: function() {
-// 		открывается летмигугл и ищет ответ 
-// 	}
-// }
-
-// var joke = {
-// 	loadSchedule: function() {
-// 		подгружает рандомные шутки в таблицу
-// 	}
-// 	showTable: function() {
-// 		показывается таблица из своей игры
-// 		выбирается рандомная ячейка
-// 		таблица исчезает
-// 	},
-// 	showJoke: function() {
-// 		появляется попап с шуткой
-// 	}
-// };
-// app.beforeAsk();
-// joke.loadSchedule();
-// $("#askButton").on("click", app.load());
-
 $(document).ready(function() {
 
-	function beforeAsk() {
+	$(".chat").on("click", function() {
+		$(".question").fadeIn(200);
+	});
+
+	// рандомые фразы перед тем как начать диалог
+
+	interval = setInterval(sayPhrase, 3000); 
+	function listener(event) {
+		if(event.keyCode == 13 || event.keyCode == 32) {
+			clearInterval(interval);
+			document.removeEventListener("keydown", listener);
+			// app.turn();
+		};
+	};
+	document.addEventListener("keydown", listener);
+
+	function sayPhrase() {
 		var phrases,
 			phrase,
 			rand,
 			interval;
 		phrases = ["one", "two", "three"];
-
-		function sayPhrase() {
-			rand =  -0.5 + Math.random() * (phrases.length);
-	    	rand = Math.round(rand);
-	    	phrase = phrases[rand];
-	    	$(".c-talk")[0].textContent = phrase;
-		};
-    interval = setInterval(sayPhrase, 3000); 
-    function listener(event) {
-			if(event.keyCode == 13 || event.keyCode == 32) {
-				clearInterval(interval);
-				document.removeEventListener("keydown", listener);
-				// app.turn();
-				// здесь рандом на поворот
-				// app.ask();
-			}
-		};
-		document.addEventListener("keydown", listener);
+		rand =  -0.5 + Math.random() * (phrases.length);
+    	rand = Math.round(rand);
+    	phrase = phrases[rand];
+    	$(".talk")[0].textContent = phrase;
+    	// тут должно всплывать облако с фразой и исчезать
 	};
 
-	function turn() {
-		// var rand,
-		// 	flag;
-		// rand = -0.5 + Math.random() * 2;
-		// rand = Math.round(rand);
-		// if(rand === 1) {
-		// 	console.log("no", flag, rand);
-		// 	// app.turn();
-		// } else {
-		// 	console.log("yes", flag, rand);
-		// 	// реально turn
-		// };
+// 	function turn() {
+// 		// поворачивается
+// 		// IDdialog1 = "Что такое?"
+// 		app.randomiser();
+// 	};
 
-		// передать turned или нет
 
-	};
+// 	function randomiser() {
+// 		var rand = Math.round(Math.random());
+// 		ask(rand);
+// 	};
 
-	function ask(turned) {
-		// он спрашивает
-		// повляется попап с инпутом
+// 	function answear() {
+// 		// преобразовать в запрос
+// 		// "http://lmgtfy.com/?q=" + question; 
+// 	};
 
-		// задаешь вопрос
 
-		if(turned) {
-			joke.showTable();
-		} else app.turn();
+// отдельно от объекта app
 
-		var question = $("#question input")[0].value;
+// function ask(predictJoke) {
+// 	$(".question__button").on("click", function() {
+// 		var questionText = $(".question__text")[0].value;
+// 		if(questionText) {
+// 			$(".chat").append('<div class="person__second">' + questionText + '</div>');
+// 			$(".question").fadeOut(100);
+// 			$(".question__text")[0].value = "";
+// 			$(".question__title")[0].textContent = "Че такое?";
+// 			if((jokesCounter === 0) || (joke.counter < 5 && predictJoke === 1) {
+// 				joke.showTable();
+// 			} else app.answear(questionText);
+// 		} else { 
+// 			$(".question__title")[0].textContent = "Так и будешь молчать?";
+// 			return;
+// 		};
+// 	});
+// });
 
-				// преобразовать в запрос
-				// "http://lmgtfy.com/?q=" + question; 
+	// var joke = {
+		// counter: 0,
+	// 	// showTable: function() {
 
-		}); 
-	};
+	// 	// },
+	// 	choose: function() {
+	// 		var rand = - 0.5 + Math.random() * 6;
+	// 		rand = Math.round(rand);
+	// 		// в таблице ячейка[rand] мигает затем исчезает текст и
+	// 		joke.show();
+	// 	}, 
+	// 	show: function() {
+	// 		var currentJoke = "json";
+	// 		$(".joke__text")[0].textContent = currentJoke;
+	// 		$(".joke__bg").show();
+	// 		function readJoke() {
+	// 			$(".joke__text")[0].textContent = "";
+	// 			$(".joke__bg").fadeOut(200);
+	// 		};
+	// 		setTimeout(readJoke, 1000);
+	// 			// попап убирается
+	// 			jokesCounter += 1;
+	// 			app.randomiser();
+	// 	}
+	// };
 });
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// function beforeAsk() {
+	// 	var phrases,
+	// 		phrase,
+	// 		rand,
+	// 		interval;
+	// 	phrases = ["one", "two", "three"];
+
+	// 	function sayPhrase() {
+	// 		rand =  -0.5 + Math.random() * (phrases.length);
+	//     	rand = Math.round(rand);
+	//     	phrase = phrases[rand];
+	//     	// тут должно всплывать облако с фразой и исчезать
+	//     	$(".c-talk")[0].textContent = phrase;
+	// 	};
+ //    	interval = setInterval(sayPhrase, 3000); 
+ //    	function listener(event) {
+	// 		if(event.keyCode == 13 || event.keyCode == 32) {
+	// 			clearInterval(interval);
+	// 			document.removeEventListener("keydown", listener);
+	// 			// app.turn();
+	// 		};
+	// 	};
+	// 	document.addEventListener("keydown", listener);
+	// };
